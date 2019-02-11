b388cbda39075e567565bc8ead157e64ce2a1a59
daniellezhang <liuxinzhang042@gmail.com>
2019-02-11 15:36:04 +1100

    comments added

--- lines.py
+++ lines.py
@@ -27,6 +27,7 @@ def main():
     plus = f.readline()
     quality_string = f.readline()
 
+    #find the location of the problematic seqs
     while header and seq and plus and quality_string:
         seq_id = header.split(' ')[0][1:]
         if seq_id in prob_seq:
@@ -40,11 +41,10 @@ def main():
     f.close()
     line_list.sort()
 
-    #create new fastq file with problematic sequences and their neighbours removed
+    #create new fastq file with problematic seqs and their neighbours removed
     command = "awk '"
     current_line = 1
     for i in line_list:
-
         end = i - n_neighbour_lines-1
         if end > current_line:
             command += format("NR==%d,NR==%d;"%(current_line,end))
