2ce57ce849e19b7657cace340e1a435dd47aa907
daniellezhang <liuxinzhang042@gmail.com>
2019-02-13 15:49:57 +1100

    add file

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -7,27 +7,41 @@ Remove duplicate headers in SAM file
 """
 
 import sys
-from subprocess import call
+import pysam
+import csv
 
 #check if the arguments are valid
 assert len(sys.argv) == 2, "need to include a SAM file to be open"
 assert sys.argv[1].split('.')[-1]=='sam', "Input file need to be SAM format"
 
-f = open(sys.argv[1],'r')
-fout = open("duplicates_removed.sam", 'w')
+f = open(sys.argv[1],"r")
+reader = csv.reader(f)
+header_lst = []
+names = []
+lengths = []
 
-header_tags = ["@HD", "@SQ", "@PG", "@RG", "@CO"]
-
-headers = []
-for line in f.readlines():
-    #not a header, add to the output file
-    if line.split(" ")[0] not in header_tags:
-        fout.write(line)
-    else:
-        #add only unique headers to the output file
-        if line not in headers:
-            headers.append(line)
-            fout.write(line)
+#create a list of unique headers
+for row in reader:
+    if not row or row[0][0:1] != '@':
+        break
+    elif row[0] not in header_lst:
+        header_lst.append(row[0])
 
 f.close()
-fout.close()
+
+#extract name and length from header
+for h in header_lst:
+    names.append(h.split('\t')[1][3:])
+    lengths.append(int(h.split('\t')[2][3:]))
+
+#create AlignmentHeader file
+header = pysam.AlignmentHeader.from_references(reference_lengths = lengths,reference_names = names)
+#create outfile with no duplicate header
+outfile = pysam.AlignmentFile("duplicates_removed.sam","w", header = header)
+samfile = pysam.AlignmentFile(sys.argv[1],"r")
+#add alignments to outfile
+for read in samfile.fetch():
+    outfile.write(read)
+
+samfile.close()
+outfile.close()
