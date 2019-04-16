d169bceada6a742a94c0e5210898784661852fb1
daniellezhang <liuxinzhang042@gmail.com>
2019-04-16 13:49:07 +1000

    bug fix for calling bash script

--- analysis.py
+++ analysis.py
@@ -8,16 +8,24 @@ fastq and SAM file"""
 import sys
 from count_lines import count
 from uniquely_mapped_read import uniquely_mapped
+from subprocess import call
 
 def main():
     #check if the arguments are valid
-    assert len(sys.argv) == 3, "Need to include the fastq file and SAM file\
-    to be analysed"
-    fastq_file = sys.argv[1]
-    sam_file = sys.argv[2]
+    assert len(sys.argv) ==2, "Need to include the SAM file to be analysed"
+    sam_file = sys.argv[1]
 
-    count(fastq_file)
     uniquely_mapped(sam_file)
+    print("number of supplementary alignment:")
+    call("samtools view -c -f 2048 "+sam_file, shell=True)
+    print("number of unaligned reads:")
+    call("samtools view -c -f 4 "+sam_file, shell=True)
+    print("number of secondary alignment:")
+    call("samtools view -c "+"./secondary.sam", shell=True)
+    print("number of primary alignment:")
+    call("samtools view -c "+"./primary.sam", shell=True)
+    call("bash ./aligned_base_count.sh " + "./primary.sam", shell = True)
+    call("bash ./remove_supplementary_files.sh",shell=True)
 
 
 if __name__ == "__main__":
